ca4733097688c47de21d32fc4b2b34ad4636ed32
fa-rebeca <devrebecafa@gmail.com>
2024-07-12 15:38:48 -0300

    Aula 05.07

--- src/ListaFuncionarios.java
+++ src/ListaFuncionarios.java
@@ -1,5 +1,71 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class ListaFuncionarios {
+    final static Collection<Funcionario> quadroFuncionarios = new ArrayList<Funcionario>();
+    final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int op;
+        do{
+            menu();
+            System.out.println("Digite o numero da operação desejada:");
+            op = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (op){
+                case 1: inserir(); break;
+                case 2: rejustarSalario(); break;
+                case 3: listar(); break;
+            }
+
+        } while (op != 4);
+
+    }
+    public static void menu(){
+        System.out.println("------- OPERAÇÕES --------");
+        System.out.println("1- Inserir Funcionário");
+        System.out.println("2- Reajustar salário de todos os Funcionários");
+        System.out.println("3- Listar Funcionário");
+        System.out.println("4- Encerrar operações");
+    }
+
+    public static void inserir(){
+    System.out.println("Insira o nome do funcionário: ");
+    String nome = scanner.nextLine().toUpperCase();
+    System.out.println("Insira o número de matrícula: ");
+    int matricula = scanner.nextInt();
+    scanner.nextLine();
+    System.out.println("Insira o salário: ");
+    double salario = scanner.nextDouble();
+    scanner.nextLine();
+
+    Funcionario f = new Funcionario(nome, matricula, salario);
+
+    quadroFuncionarios.add(f);
+    }
+
+    public static void rejustarSalario(){
+        System.out.println("Digite o percentual de reajuste: ");
+        double percentual = scanner.nextDouble();
+        scanner.nextLine();
+
+        Iterator<Funcionario> it = quadroFuncionarios.iterator();
+        while (it.hasNext()){
+            Funcionario f = it.next();
+            f.reajustarSalario(percentual);
+        }
+    }
+
+    public static void listar(){
+        Iterator<Funcionario> it = quadroFuncionarios.iterator();
+
+        System.out.println("Listar: ");
+
+        for(int i = 1; it.hasNext(); i++){
+            Funcionario f = it.next();
+            System.out.println("Funcionário (" + i + "): " + f);
+        }
     }
 }
