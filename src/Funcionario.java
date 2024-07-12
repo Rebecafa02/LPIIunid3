81743799ad5a4aa97034cd68156089f9ff388b8b
fa-rebeca <devrebecafa@gmail.com>
2024-07-12 14:55:18 -0300

    Aula 05.07

---
+++ src/Funcionario.java
@@ -0,0 +1,45 @@
+public class Funcionario {
+    private String nome;
+    private int matricula;
+    private double salario;
+
+    public Funcionario (String n, int m, double s){
+        nome = n;
+        matricula = m;
+        salario = s;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String n){
+        nome = n;
+    }
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public void setSalario(double salario){
+        if (salario > 0)
+        this.salario = salario;
+    }
+
+    public int getMatricula(){
+        return matricula;
+    }
+
+    public void setMatricula(int m){
+        matricula = m;
+    }
+
+    public void reajustarSalario(double percentual){
+        double reajuste = salario * percentual/100;
+        salario += reajuste;
+    }
+
+    public String toString(){
+        return "Nome: " + nome + ", Matricula: " + matricula + ", Salario: " + salario;
+    }
+}
---
+++ src/ListaFuncionarios.java
@@ -0,0 +1,5 @@
+public class ListaFuncionarios {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
